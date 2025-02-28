f13411787320e0c9197b0f628a40b9ea559a19c2
carluza <111885660+carluza@users.noreply.github.com>
2025-02-28 04:31:02 +0000

    commit 1

--- src/models.py
+++ src/models.py
@@ -1,53 +1,39 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, Float, ForeignKey, Table
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import relationship
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.orm import relationship, declarative_base
+from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Profile (Base):
-    __tablename__ = 'profiles'
+class Planet(Base):
+    __tablename__ = 'planets'
     id = Column(Integer, primary_key=True)
-    username = Column(String, unique=True, nullable=False)
     name = Column(String, nullable=False)
-    #relacion con Story de 1 a muchos
-    stories = relationship("Story", back_populates="profile")
-    #relacion con Post de 1 a muchos
-    posts = relationship("Post", back_populates="profile")
+    climate = Column(String, nullable=False)
+    population = Column(Integer, nullable=False)
+    residents = relationship("People", back_populates="homeworld")
 
-class Story(Base):
-    __tablename__ = 'stories'
+class People(Base):
+    __tablename__ = 'peoples'
     id = Column(Integer, primary_key=True)
-    views = Column(Integer)
-    comments = Column(Integer)
-    tags = Column(String)
-    music = Column(String)
-    likes = Column(Integer, default=0)
-    profile_id = Column(Integer, ForeignKey("profiles.id"), nullable=False)
-    #relacion inversa con Profile
-    profile = relationship("Profile", back_populates="stories")
+    name = Column(String, nullable=False)
+    gender = Column(String, nullable=False)
+    height = Column(Integer, nullable=False)
+    planet_id = Column(Integer, ForeignKey("planets.id"), nullable=False)
+    homeworld = relationship("Planet", back_populates="residents")
+    vehicles = relationship("Vehicle", back_populates="pilot")
 
-
-class Post(Base):
-    __tablename__ = "posts"
+class Vehicle(Base):
+    __tablename__ = 'vehicles'
     id = Column(Integer, primary_key=True)
-    comments = Column(Integer)
-    tags = Column(String)
-    music = Column(String)
-    likes = Column(Integer, default=0)
-    profile_id = Column(Integer, ForeignKey("profiles.id"), nullable=False)
-    #relacion inversa con Profile
-    profile = relationship("Profile", back_populates="posts")
+    name = Column(String, nullable=False)
+    passengers = Column(Integer, nullable=False)
+    pilot_id = Column(Integer, ForeignKey("peoples.id"), nullable=False)
+    pilot = relationship("People", back_populates="vehicles")
 
-class Chat(Base):
-    __tablename__ = "chats"
-    id = Column(Integer, primary_key=True)
-    messages = Column(String)
-    sender_id = Column(Integer, ForeignKey("profiles.id"), nullable=False)
-    receiver_id = Column(Integer, ForeignKey("profiles.id"), nullable=False)
+    def to_dict(self):
+        return {}
 
-try:
-    render_er(Base, "diagram.png")
-    print("Diagrama UML generado con éxito como 'diagram.png'")
-except Exception as e:
-    print(f"Error al generar diagrama: {e}")
+render_er(Base, 'diagram.png')
